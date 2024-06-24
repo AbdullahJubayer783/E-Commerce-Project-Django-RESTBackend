23f425fc32c72d8d5aedfd1b89e893871a64116e
Abdullah jubayer <abdullahjubayer783@gmail.com>
2024-06-24 13:23:21 +0600

    PostgreSQL Added

--- backend_api/settings/production.py
+++ backend_api/settings/production.py
@@ -1,3 +1,15 @@
 from .base import *
 
 ALLOWED_HOSTS = ["*"]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("NAME"),
+        'USER': env("USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("HOST"),
+        'PORT': env("PORT"),
+    }
+}
